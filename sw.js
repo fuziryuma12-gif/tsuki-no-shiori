3fb0bf4d6fd50ac8f7fc6b1c90c3c4361037c50a
fuziryuma12-gif <fuziryuma12@gmail.com>
2026-08-25 02:29:03 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,13 +8,13 @@
      古い一覧を見せてしまうより、つながらないと分かる方がよい。
    ========================================================== */
 
-const CACHE = 'shiori-v3';
+const CACHE = 'shiori-v6';
 
 const SHELL = [
   './',
   './index.html',
-  './styles.css?v=3',
-  './app.js?v=3',
+  './styles.css?v=6',
+  './app.js?v=6',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
